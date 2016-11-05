43e4464890346db1fcee95aadbb72b7febff1300
Taffarel Vitor <taffareloliveira94@gmail.com>
2016-11-05 20:29:00 -0200

    Get e Post

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -26,7 +26,8 @@ function entrar(){
 // function entrar(){
 //     var enviado_user = $("#input-user").val();
 //     var enviado_pass = $("#input-pass").val();
-// 	$.post("autenticar.php",{user: enviado_user, pass: enviado_pass}, function(data){
+//     var enviado_token = $("#input-token").val();
+// 	$.post("autenticar.php",{user: enviado_user, pass: enviado_pass, _token: enviado_token}, function(data){
 // 	   alert("Olá");
 //        console.log(data);
 
